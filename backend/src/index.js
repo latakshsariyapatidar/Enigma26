5c4f19f51f0f814707ff23eca213e6f97bc52fe1
Op-KriSh <krish.012.gupta@gmail.com>
2026-03-05 00:34:29 +0530

    basic setup backend

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import dotev from 'dotenv';
+import { connectDB } from './db/index.js';
+import app from './app.js';
+
+dotev.config({
+    path: './.env'
+});
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+});
