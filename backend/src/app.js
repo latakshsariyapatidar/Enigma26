5b7690518fcb3a7c9042206d67c5fc6d2cdea348
Op-KriSh <krish.012.gupta@gmail.com>
2026-03-06 16:30:27 +0530

    team features and services completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import errorController from './controllers/errorController.js';
 import { ApiResponse } from './utils/apiResponse.js';
-import { verifyJWT } from './middlewares/authMiddleware.js';
+
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN || '*',
@@ -15,9 +16,16 @@ app.use(express.urlencoded({
     extended: true,
     limit: '100kb'
 }));
-app.use(verifyJWT);
+app.use(cookieParser());
 app.use('/healthcheck', (req, res) => {
     res.status(200).json(new ApiResponse(200, null, 'OK'));
 });
+
+import userRouter from './routes/teamRoutes.js';
+app.use('/api/v1/users', userRouter);
+
+
+
+
 app.use(errorController);
 export default app;
---
+++ backend/src/routes/teamRoutes.js
@@ -0,0 +1,15 @@
+import {loginUser, logoutUser, changePassword, getCurrentUser, refreshAccessToken, signupUser,deleteUser} from "../controllers/teamController.js";
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/authMiddleware.js";
+const userRouter = Router();
+
+userRouter.route("/signup").post( signupUser);
+userRouter.route("/login").post(loginUser);
+userRouter.route("/logout").post(verifyJWT, logoutUser);
+userRouter.route("/change-password").post(verifyJWT, changePassword);
+userRouter.route("/current").get(verifyJWT, getCurrentUser);
+userRouter.route("/refresh-token").post(verifyJWT,refreshAccessToken);
+userRouter.route("/delete").delete(verifyJWT, deleteUser);
+
+
+export default userRouter;
