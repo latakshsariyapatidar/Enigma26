3291680618112cface5934633f8de38ea7f50a5c
Op-KriSh <krish.012.gupta@gmail.com>
2026-03-06 11:50:05 +0530

    authentication methods and middleware completed

--- backend/src/models/teamModel.js
+++ backend/src/models/teamModel.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import validator from "validator";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const teamSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -20,31 +21,50 @@ const teamSchema = new mongoose.Schema({
     minlength: 8,
     select: false,
   },
-  passwordConfirm: {
-    type: String,
-    required: [true, `Please confirm your password!`],
-    //this only works on SAVE or CREATE -- So whenever we want to update a user's credentials this validation wont work
-    validate: {
-      validator: function (val) {
-        return this.password === val;
-      },
-      message: "The passwords don't match, Please re-enter the passwords.",
-    },
-  },
-  changedPasswordAt: Date,
-  createdAt: Date,
-  passwordResetToken: {
-    type: String,
-  },
-  passwordResetExpires: {
-    type: Date,
-  },
   role: {
     type: String,
     enum: ["admin", "participant"],
     default: "participant",
   },
+  refreshToken:{
+        type:String
+  },
 });
+
+
+
+teamSchema.pre("save",async function (){
+     if(!this.isModified("password")) return ;
+    this.password = await bcrypt.hash(this.password, 10) 
+});
+
+teamSchema.methods.correctPassword = async function (Password) {
+  return await bcrypt.compare(Password, this.password);
+};
+
+teamSchema.methods.generateAccessToken=function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        name:this.name,
+        role:this.role
+    },process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+
+teamSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id:this._id,
+    },process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
 
 const Team = mongoose.model("Team", teamSchema);
 export default Team;
