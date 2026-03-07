0cf8ea3aeb20474ab39a996f862fa0ec0167500e
Jayesh <980jayesh@gmail.com>
2026-03-07 22:45:05 +0530

    added a response for completed in getTeamProgress, edited the scoring , commented extra code in teamProgressController, added adminAuth in adminRoutes , location failure wasnt checked in a few places-- so fixed it

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,13 +4,14 @@ import {
   getAllTeamProgressAdmin,
 } from "../controllers/teamProgressController.js";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
+import { isAdmin } from "../middlewares/validationMiddleware.js";
 
 const router = express.Router();
 
 // full progress of one team
-router.get("/team/:teamId", verifyJWT, getTeamProgressAdmin); // ! Need to add Is admin middleware here (VERY IMPORTANT)
+router.get("/team/:teamId", verifyJWT, isAdmin, getTeamProgressAdmin); // ! Need to add Is admin middleware here (VERY IMPORTANT)
 
 // leaderboard + dashboard
-router.get("/dashboard", verifyJWT, getAllTeamProgressAdmin);
+router.get("/dashboard", verifyJWT, isAdmin, getAllTeamProgressAdmin);
 
 export default router;
--- backend/src/routes/teamProgressRoutes.js
+++ backend/src/routes/teamProgressRoutes.js
@@ -2,9 +2,6 @@ import express from "express";
 import {
   getTeamProgress,
   getClueHint,
-  getPuzzle,
-  getPuzzleHint,
-  submitAnswer,
 } from "../controllers/teamProgressController.js";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 
@@ -12,8 +9,5 @@ const router = express.Router();
 
 router.get("/progress", verifyJWT, getTeamProgress);
 router.get("/clueHint", verifyJWT, getClueHint);
-router.get("/puzzle", verifyJWT, getPuzzle);
-router.get("/puzzleHint", verifyJWT, getPuzzleHint);
-router.post("/submitAnswer", verifyJWT, submitAnswer);
 
 export default router;
